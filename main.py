0a6a36fb1fc4bfe76e8ca09d04477865648da362
Magdin Stoica <magdin.stoica@sheridancollege.ca>
2025-12-03 15:58:51 -0500

    Protect the application from unhandled exceptions

--- main.py
+++ main.py
@@ -1,3 +1,13 @@
 """Boot-strapping module to create the application and ask it to run"""
 
 from application import Application
+
+try:
+    #create the application object
+    app = Application()
+
+    #ask the application to run
+    app.run()
+except Exception as ex:
+    print(f"An unknown eror has occourred.\nMessage: {ex}")
+
