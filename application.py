dc072a7ef499b262cbfc8e93f9a5b4dd46f159c0
Magdin Stoica <magdin.stoica@sheridancollege.ca>
2025-12-03 15:59:06 -0500

    Implement the association relationship between Application and Competition

--- application.py
+++ application.py
@@ -6,7 +6,8 @@ from competition import Competition
 
 class Application:
     def __init__(self):
-        pass
+        #implement the HAS-A relationship between Application and Competition
+        self._competition = Competition()
 
     def run(self):
         """Provides the main interaction loop of the program"""
