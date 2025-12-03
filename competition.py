fe52073e831728a43606b51a90e4005da9ab7254
Magdin Stoica <magdin.stoica@sheridancollege.ca>
2025-12-03 15:30:25 -0500

    Define the structure of the competition class and its list of athletes.

--- competition.py
+++ competition.py
@@ -3,4 +3,20 @@
 from athelete import Athlete
 
 class Competition:
-    pass
+    ATHLETE_DATA_FILE_NAME = "data/olympics.csv"
+
+    def __init__(self):
+        """Constructor method that defines the field variables for competition objects"""
+        self._athleteList = [] # 1 to many (*) HAS-A relationship
+
+    def load(self):
+        """Loads the athlete data from a data file"""
+        pass
+
+    def save(self):
+        """Saves the athlete data to a data file"""
+        pass
+
+    def printCompetitors(self):
+        """Prints the list of competitors that participate in the competition"""
+        pass
